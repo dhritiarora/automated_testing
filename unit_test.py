4c17e59454891173df479a346a61da3f66bb81b9
akankshadhyani05 <69378583+akankshadhyani05@users.noreply.github.com>
2021-07-09 10:52:06 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-df=pd.read_csv("D:/Downloads/df.csv")
+df=pd.read_csv("df.csv")
 
 def test_no_of_columns():
     assert (list(df.columns)) == ['Unnamed: 0','amt', 'city_pop', 'trans_hour', 'age', 'food_dining', 'gas_transport', 'grocery_net', 'grocery_pos', 'health_fitness', 'home','kids_pets', 'misc_net',  'misc_pos', 'personal_care', 'shopping_net', 'shopping_pos', 'travel', 'gender_M', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday', 'Sunday','count_trans_90d','count_trans_24h', 'category_type_count','merchant_count','total_amt_30d','avg_amt_30d','amt_category_30d','merchant_amt_30d','amt_24h', 'is_fraud']
